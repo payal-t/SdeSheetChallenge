d12a70e6b57b81c7dd376f969962ab198633f5fb
payal-t <71988321+payal-t@users.noreply.github.com>
2022-08-26 23:51:34 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,32 @@
+//package Arrays;
+//
+//
+//import java.util.ArrayList;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        int[] evenArray={1,2,3,5,8,9,2,8,6};
+//
+//        ArrayList<Integer> evenLists=new ArrayList<Integer>();
+//
+//      // evenList.forEach(n -> System.out.println(n));
+//
+//       //evenList.forEach(n -> { if(evenArray[n]%2==0) System.out.println(evenArray);});
+//
+//        for(int i=0;i<evenLists.length;i++)
+//        {
+//            if(Arrays.evenLists[i]%2==0)
+//            {
+//                evenLists.add(evenArray[i]);
+//            }
+//            else{
+//                System.out.println("remaining elements");
+//            }
+//        }
+//
+//
+//    }
+//
+//
+//
+//}
